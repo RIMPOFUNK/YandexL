20c0839fb4eb2d9a0233b90da7eecb10df634052
Madaspe <39723891+Madaspe@users.noreply.github.com>
2019-03-31 10:52:51 +0300

    OOП

--- Урок 'Введение в ООП. Полиморфизм'/Форматы дат.py
+++ Урок 'Введение в ООП. Полиморфизм'/Форматы дат.py
@@ -1,24 +1,78 @@
 class AmericanDate:
     def __init__(self, *args):
-        args = list(args)
-        if len(str(args[1])) == 1:
-            args[1] = '0' + str(args[1])
-        if len(str(args[2])) == 1:
-            args[2] = '0' + str(args[2])
-        self.forma = str(args[1]) + "." + str(args[2]) + "." + str(args[0])
+        self.year = args[0]
+        self.month = args[1]
+        self.day = args[2]
+        self.result = format(self)
+
+    def set_year(self, year):
+        self.year = year
+        self.result = format(self)
+
+    def set_month(self, month):
+        self.month = month
+        self.result = format(self)
+
+    def set_day(self, day):
+        self.day = day
+        self.result = format(self)
 
     def format(self):
-        return self.forma
+        if len(str(self.day)) == 1:
+            self.day = '0' + str(self.day)
+        if len(str(self.month)) == 1:
+            self.month = '0' + str(self.month)
+        return str(self.month) + "." + str(self.day) + "." + str(self.year)
+
+    def get_month(self):
+        if self.month[0] == "0":
+            return self.month[1]
+        return self.month
+
+    def get_day(self):
+        if self.day[0] == "0":
+            return self.day[1]
+        return self.day
+
+    def get_year(self):
+        return self.year
 
 
 class EuropeanDate:
     def __init__(self, *args):
-        args = list(args)
-        if len(str(args[1])) == 1:
-            args[1] = '0' + str(args[1])
-        if len(str(args[2])) == 1:
-            args[2] = '0' + str(args[2])
-        self.forma = str(args[2]) + "." + str(args[1]) + "." + str(args[0])
+        self.year = args[0]
+        self.month = args[1]
+        self.day = args[2]
+        self.result = format(self)
+
+    def set_year(self, year):
+        self.year = year
+        self.result = format(self)
+
+    def set_month(self, month):
+        self.month = month
+        self.result = format(self)
+
+    def set_day(self, day):
+        self.day = day
+        self.result = format(self)
 
     def format(self):
-        return self.forma
+        if len(str(self.day)) == 1:
+            self.day = '0' + str(self.day)
+        if len(str(self.month)) == 1:
+            self.month = '0' + str(self.month)
+        return str(self.day) + "." + str(self.month) + "." + str(self.year)
+
+    def get_month(self):
+        if self.month[0] == "0":
+            return self.month[1]
+        return self.month
+
+    def get_day(self):
+        if self.day[0] == "0":
+            return self.day[1]
+        return self.day
+
+    def get_year(self):
+        return self.year
