57cb9997ae5b104a2987dc6aa03024fc8f55065d
Madaspe <39723891+Madaspe@users.noreply.github.com>
2019-04-09 11:34:59 +0300

    Add code "Вывод предложений"

---
+++ Урок 'Введение в ООП. Полиморфизм'/Вывод предложений.py
@@ -0,0 +1,37 @@
+class Paragraph:
+    def __init__(self, width):
+        self.width = width
+        self.words = []
+
+    def add_word(self, word):
+        self.words.append(word)
+
+
+class LeftParagraph(Paragraph):
+    def end(self):
+        line = ''
+        for word in self.words:
+            if not line:
+                line += word
+            elif len(line) + len(word) + 1 <= self.width:
+                line += ' ' + word
+            else:
+                print(line)
+                line = word
+        print(line)
+        self.words.clear()
+
+
+class RightParagraph(Paragraph):
+    def end(self):
+        line = ''
+        for word in self.words:
+            if not line:
+                line += word    
+            elif len(line) + len(word) + 1 <= self.width:
+                line += ' ' + word
+            else:
+                print(line.rjust(self.width, ' '))
+                line = word
+        print(line.rjust(self.width, ' '))
+        self.words.clear()
--- Урок 'Введение в ООП. Полиморфизм'/Прямоугольники.py
+++ Урок 'Введение в ООП. Полиморфизм'/Прямоугольники.py
@@ -1,2 +1,10 @@
 class Rectangle:
-    pass
+    def __init__(self, x, y, w, h):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+
+    def intersection(self, class_rectangle):
+        if isinstance(class_rectangle, Rectangle):
+
